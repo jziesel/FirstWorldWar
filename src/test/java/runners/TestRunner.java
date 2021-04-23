becdf30aa996ba5c47cbff291ad9b60246e5a8c5
James Ziesel <jziesel@carolina.rr.com>
2021-04-23 14:38:41 -0400

    Step 13 ->> Incorporated TestNG .jar to project; added extent.properties; updated pom.xml dependencies for ExtentReports compatibility; updated TestRunner to replace JUnit libs with TestNG.

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,20 +1,15 @@
 package runners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-//import com.cucumber.listener.Reporter;
-//import org.testng.annotations.AfterClass;
-
-@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources",
         glue = {"stepDefinitions"},
-        plugin = {"pretty", "html:target/cucumber-reports/Report.html"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true,
         tags = "@Regression"
 )
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
 }
